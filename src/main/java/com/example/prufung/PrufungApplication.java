7cbc69b8fc8d704011b88c59e28af79855e7765f
Azur <azur.ostrakovic@gmail.com>
2023-06-13 11:24:46 +0200

    Initial commit

---
+++ src/main/java/com/example/prufung/PrufungApplication.java
@@ -0,0 +1,13 @@
+package com.example.prufung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrufungApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PrufungApplication.class, args);
+    }
+
+}
